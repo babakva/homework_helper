b1d98ecabdccebcaef704483601ab86d53915369
babak <babak@Babaks-MBP.lan>
2024-04-23 10:57:38 +0200

    added courses section in parent view

--- pages/parent.py
+++ pages/parent.py
@@ -13,7 +13,7 @@ with st.sidebar:
 st.write(
     """Här kan du se hur det går för ditt barn."""
 )
-st.header('Den här veckas läxor', divider='rainbow')
+st.header('Den här veckans läxor', divider='rainbow')
 df = pd.DataFrame(
     {
         "homework": ["Planeten Jupiter", "Ronja Rövardotter", "Kejsare Caesar"],
@@ -47,7 +47,37 @@ st.dataframe(
     },
     hide_index=True,
 )
-st.divider()
+st.header('Ditt barns kursplaner', divider='rainbow')
+df2 = pd.DataFrame(
+    {
+        "name": ["Svenska", "Matematik", "Biologi"],
+        "url": ["https://www.skolverket.se/undervisning/grundskolan/laroplan-och-kursplaner-for-grundskolan/laroplan-lgr22-for-grundskolan-samt-for-forskoleklassen-och-fritidshemmet?url=907561864%2Fcompulsorycw%2Fjsp%2Fsubject.htm%3FsubjectCode%3DGRGRSVE01%26tos%3Dgr&sv.url=12.5dfee44715d35a5cdfa219f",
+         "https://www.skolverket.se/undervisning/grundskolan/laroplan-och-kursplaner-for-grundskolan/laroplan-lgr22-for-grundskolan-samt-for-forskoleklassen-och-fritidshemmet?url=907561864%2Fcompulsorycw%2Fjsp%2Fsubject.htm%3FsubjectCode%3DGRGRMAT01%26tos%3Dgr&sv.url=12.5dfee44715d35a5cdfa219f",
+          "https://www.skolverket.se/undervisning/grundskolan/laroplan-och-kursplaner-for-grundskolan/laroplan-lgr22-for-grundskolan-samt-for-forskoleklassen-och-fritidshemmet?url=907561864%2Fcompulsorycw%2Fjsp%2Fsubject.htm%3FsubjectCode%3DGRGRBIO01%26tos%3Dgr&sv.url=12.5dfee44715d35a5cdfa219f"],
+        "grade": [random.choice(['A', 'B', 'C', 'D', 'E']) for _ in range(3)],
+        "next_exam": [random.randint(0, 30) for _ in range(3)],
+    }
+)
+st.dataframe(
+    df2,
+    column_config={
+        "name": "Kurs",
+        "grade": st.column_config.TextColumn(
+            "Betyg",
+            help="Nuvarande betyg",
+        ),
+        "url": st.column_config.LinkColumn("Länk", display_text="Gå till kursplanen"),
+        "next_exam": st.column_config.NumberColumn(
+            "Nästa prov",
+            help="Nästa prov",
+            format="Om %d dagar",
+        ),
+
+    },
+    hide_index=True,
+)
+
+
 options = ["Lägg till läxor", "Se vad barnet har svarat", "Rekommendationer för ditt barn"]
 option = st.selectbox(
    "Vad skulle du vilja göra",
